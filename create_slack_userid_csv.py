17e06d31a659c45a2df983d92862a189c9a73e61
Michael Fink <65927705+AnubisRooster@users.noreply.github.com>
2023-09-13 19:21:39 -0400

    Update create_slack_userid_csv.py

--- create_slack_userid_csv.py
+++ create_slack_userid_csv.py
@@ -36,14 +36,10 @@ def start():
 
         #for user in result["members"]:
         for user in result:
-            #print(user, end='\n\n\n')
-            #{'id': 'U026GDM4B25', 'team_id': 'T02G42F7P', 'name': 'tbooker-ctr', 'deleted': True, 'profile': {'title': 'Procurement', 'phone': '5037013052', 'skype': '', 'real_name': 'Toni Booker', 'real_name_normalized': 'Toni Booker', 'display_name': 'tbooker-ctr', 'display_name_normalized': 'tbooker-ctr', 'fields': None, 'status_text': '', 'status_emoji': '', 'status_emoji_display_info': [], 'status_expiration': 0, 'avatar_hash': 'b097db3e7a39', 'image_original': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_original.jpg', 'is_custom_image': True, 'first_name': 'Toni', 'last_name': 'Booker', 'image_24': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_24.jpg', 'image_32': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_32.jpg', 'image_48': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_48.jpg', 'image_72': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_72.jpg', 'image_192': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_192.jpg', 'image_512': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_512.jpg', 'image_1024': 'https://avatars.slack-edge.com/2021-06-28/2219465672691_b097db3e7a39662e6f93_1024.jpg', 'status_text_canonical': '', 'team': 'T02G42F7P'}, 'is_bot': False, 'is_app_user': False, 'updated': 1631619826}
-
             slack_user_id = user["id"]
             user_csl = user["name"]
             real_name = user["profile"]["real_name"]
             deleted_user = user["deleted"] # return true or false. true for users that have been deactivated
-            #print(real_name)
 
 
             # write user_id slack data to csv file if they are not in deleted/deactivated state
@@ -72,7 +68,7 @@ def create_csv_file_for_slack_userid_data(filename, fields):
 def write_userid_list_to_csv(filename, slack_user_id, user_csl, real_name):
     #print(slack_user_id, user_csl,real_name)
     '''
-    U026GDM4B25 tbooker-ctr Toni Booker
+    U026GDM4B26 dbooker Danny Booker
     '''
 
     # data rows of csv file 
